d9b43ee298c12c0755392fabdd001214fa6a8d28
Comunityblogs <elpapurri2018k@gmail.com>
2022-05-25 10:31:00 -0500

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+let allMusic = [
+  {
+    name: "Nightcore - Blue (Da Ba Dee)",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Aaron Smith - Dancin",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "MBB - Clouds",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "vengeance - trap beat",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Heart Shaped box",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Nightcore - Gangsta's Paradise",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Lighting Up The Dark",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Nightcore - Memories ",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Nightcore - In my dreams",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Romy Wave - Light Out",
+    artist: "Music For Youtube - Magic Sound",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Imagine Dragons & J.I.D - Enemy",
+    artist: "KingMusic Official",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "The Neighbourhood - Sweater Weather",
+    artist: "Remarkable Music",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Sweet Dreams - The Remix Cartell",
+    artist: "The WinFexX - La mejor Musica Sin Copyright",
+    img: "music-13",
+    src: "music-13"
+  },
+];
